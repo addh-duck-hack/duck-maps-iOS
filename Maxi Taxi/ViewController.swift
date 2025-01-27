5b5f7bbd66865cbb499ef85f1bdc3d9051486caa
Adrian Jacobo <adrian.jacobo@wallia.io>
2025-01-27 13:43:35 -0600

    Inicio de Proyecto

---
+++ Maxi Taxi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maxi Taxi
+//
+//  Created by Gerardo Jacobo Luna on 21/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Maxi TaxiTests/Maxi_TaxiTests.swift
@@ -0,0 +1,17 @@
+//
+//  Maxi_TaxiTests.swift
+//  Maxi TaxiTests
+//
+//  Created by Gerardo Jacobo Luna on 21/11/24.
+//
+
+import Testing
+@testable import Maxi_Taxi
+
+struct Maxi_TaxiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
